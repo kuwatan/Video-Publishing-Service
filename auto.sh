a03082afdc59568811405810bedebed72d40f89f
kuwatan <160240345+kuwatan@users.noreply.github.com>
2025-02-07 22:19:56 +0900

    完成

--- auto.sh
+++ auto.sh
@@ -7,21 +7,38 @@ echo "basic認証を行いますか？ 行う=0 行わない=1"
 read bas
 echo "プログラムを実行します"
 
+#パスワード有りの方
 if [ $bas -eq 0 ]; then
 	echo "ユーザー名は?"
 	read user
 	echo "パスワードは?"
 	read pas
-	htpasswd -b -c /etc/nginx/.htpasswd$name/$user $pas
+	#パスワードハッシュのやつ
+	htpasswd -b -c /etc/nginx/.htpasswd$name $user $pas
+	#ディレクトリ作成
 	mkdir /var/www/html/$name
 	sleep 1.5s
+	#メインファイルコピー
 	cp /var/www/html/moto/index.html /var/www/html/$name
+	#パスワードをnginxに設定
+	sed -i '126a \        \location '/$name' {\
+	auth_basic            "Basic Auth";\
+	auth_basic_user_file  "'/etc/nginx/.htpasswd$name'";\
+	}
+		' /etc/nginx/sites-available/default
+	#ホームファイルに新規追加
+	sed -i '6a\        \"'$name'" => "'/$name/'",' /var/www/html/index.php
+	systemctl reload nginx
 	echo "サイト番号$nameへの処理が完了しました"
-	echo "index.phpとNginxへの書き込みを忘れずに"
+
+#パスワード無しの方
 elif [ $bas -eq 1 ]; then
+	#ディレクトリ作成
 	mkdir /var/www/html/$name
         sleep 1.5s
-        cp /var/www/html/moto/index.html /var/www/html/$name
-        echo "サイト番号$nameへの処理が完了しました"
-	echo "index.phpとNginxへの書き込みを忘れずに"
+        #メインファイルコピー
+	cp /var/www/html/moto/index.html /var/www/html/$name
+	#ホームファイルに新規追加
+	sed -i '6a\        \"'$name'" => "'/$name/'",' /var/www/html/index.php
+	echo "サイト番号$nameへの処理が完了しました"
 fi
